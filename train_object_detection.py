b396d88cfa761e621e107831ef4f9dfdbc87af0c
Henry <sensork82@gmail.com>
2025-04-16 19:58:17 +0000

    Update train_object_detection.py

--- train_object_detection.py
+++ train_object_detection.py
@@ -457,12 +457,12 @@ def main():
      dataset = load_dataset(args.dataset_name, cache_dir=args.cache_dir, trust_remote_code=args.trust_remote_code)
      print(dataset)
 #     # If we don't have a validation split, split off a percentage of train as validation.
-#     args.train_val_split = None if "validation" in dataset.keys() else args.train_val_split
-#     if isinstance(args.train_val_split, float) and args.train_val_split > 0.0:
-#         split = dataset["train"].train_test_split(args.train_val_split, seed=args.seed)
-#         dataset["train"] = split["train"]
-#         dataset["validation"] = split["test"]
-
+     args.train_val_split = None if "validation" in dataset.keys() else args.train_val_split
+     if isinstance(args.train_val_split, float) and args.train_val_split > 0.0:
+         split = dataset["train"].train_test_split(args.train_val_split, seed=args.seed)
+         dataset["train"] = split["train"]
+         dataset["validation"] = split["test"]
+     print(dataset)
 #     # Get dataset categories and prepare mappings for label_name <-> label_id
 #     categories = dataset["train"].features["objects"].feature["category"].names
 #     id2label = dict(enumerate(categories))
